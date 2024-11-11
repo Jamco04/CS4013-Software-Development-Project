66b4ccbeb1ba27e4cd4d112e5ec79626c315e842
Adam Urban <99017970+adamurban04@users.noreply.github.com>
2024-11-11 19:38:08 +0000

    payrollSystem java Project

---
+++ PayrollSystem/src/main/java/com/mycompany/payrollsystem/Staff.java
@@ -0,0 +1,22 @@
+package com.mycompany.payrollsystem;
+
+
+public class Staff {
+
+    private String title;
+    private int scalePoint;
+    private double annualRate;
+    
+    
+    
+    public Staff(){
+    }
+    
+    public Staff(String title, int scalePoint){
+        this.title = title;
+        this.scalePoint = scalePoint;
+
+    }
+    
+    
+}
