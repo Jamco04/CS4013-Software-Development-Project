2ce14bb2684634c752ee7bb52b40d461e4c8331a
Jamco04 <jamco2004@gmail.com>
2024-11-19 19:47:50 +0000

    Creation of part time employee constructor

--- src/main/java/com/mycompany/payrollsystem/staff/parttime/PartTimeEmployee.java
+++ src/main/java/com/mycompany/payrollsystem/staff/parttime/PartTimeEmployee.java
@@ -4,6 +4,12 @@ package com.mycompany.payrollsystem.staff.parttime;
 import com.mycompany.payrollsystem.staff.Staff;
 
 public class PartTimeEmployee extends Staff {
-    
-    
+    private double hourlyRate;
+    private double hoursWorked;
+
+    public PartTimeEmployee(String title, int scalePoint, double hourlyRate, double hoursWorked){
+        super(title,scalePoint);
+        this.hourlyRate = hourlyRate;
+        this.hoursWorked= hoursWorked;
+    }
 }
