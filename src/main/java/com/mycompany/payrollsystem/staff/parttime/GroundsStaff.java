9c1305cc9d0ea32f754a249f6e959566d82ba01c
Jamco04 <jamco2004@gmail.com>
2024-11-19 20:38:48 +0000

    Fixed part time employee constructors to work with updated staff datafields

--- src/main/java/com/mycompany/payrollsystem/staff/parttime/GroundsStaff.java
+++ src/main/java/com/mycompany/payrollsystem/staff/parttime/GroundsStaff.java
@@ -5,4 +5,4 @@ public class GroundsStaff extends PartTimeEmployee {
     public GroundsStaff(String name, int id, String title, int scalePoint, double hourlyRate, double hoursWorked) {
         super(name, id, title, scalePoint, hourlyRate, hoursWorked);
     }
-}
+}// test
