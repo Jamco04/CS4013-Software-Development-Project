5ba91af9a08301d9157a09d5171689720af10c8c
endabuckley <endabuckley01@gmail.com>
2024-11-28 20:00:09 +0000

    implemented check for valid title when adding employees in admin class

--- src/main/java/com/mycompany/payrollsystem/system/user/Admin.java
+++ src/main/java/com/mycompany/payrollsystem/system/user/Admin.java
@@ -8,7 +8,8 @@ import com.mycompany.payrollsystem.system.PayLoader;
 import com.mycompany.payrollsystem.system.StaffContainer;
 import java.io.FileWriter;
 import java.io.IOException;
-
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Admin {
@@ -36,6 +37,24 @@ public class Admin {
     }
 
     public void addStaff() {
+        List<String> validTitles = Arrays.asList(
+                "Full Professor","Professor","Associate Professor A","Associate Professor B",
+                "Assistant Professor","Teaching Assistant","Senior Administrative Officer III",
+                "Senior Administrative Officer II","Senior Administrative Officer I","Senior Executive Administrator",
+                "Executive Administrator","Senior Administrator","Administrator","EPS Portfolio Manager",
+                "EPS Category Manager","EPS Category Specialist Higher",
+                "EPS Category Specialist","Sub Librarian","Assistant Librarian II","Assistant Librarian I","Senior Library Assistant",
+                "Library Assistant","Analyst Programmer III","Analyst Programmer II",
+                "Analyst Programmer I","Senior Computer Operator","Computer Operator","Print Operator II","Print Operator I","Computer Lab Attendant",
+                "Temporary Computer Assistant","Chief Technical Officer",
+                "Technical Officer","Senior Technical Officer","Senior Lab Attendant","Laboratory Attendant","Sen Porter/Attendant","Porter/Attendant",
+                "Grounds Supervisor","Groundsworkperson","Senior Aide","Groundsworkperson Machine Attendant","Service Staff","Service Staff Shift",
+                "Plant Maintenance Aide","Grounds Foreperson","Teaching Fellow","University Teacher","Associate Teacher",
+                "Therapies Regional Supervisors Regional Placement Facilitator","Clinical Tutor","Clinical Fellow","Assistant Senior Instructor",
+                "Lead Instructor","Multi Activity Instructor (Grade I)","Multi Activity Instructor (Grade II)","Multi Activity Instructor (Grade III)",
+                "Assistant Instructor","CO-OP STUDENTS");
+
+        List<String> validCategories = Arrays.asList();
 
         // Staff Type
         int type = readInt("Enter staff type: (1) full-time (2) part-time:");
@@ -59,7 +78,15 @@ public class Admin {
         }
 
         // Title
-        String title = readString("Enter staff title:");
+        String title = "";
+        while (true) {
+            title = readString("Enter staff title:").trim();
+            if (validTitles.contains(title)) {
+                break;
+            } else {
+                System.out.println("Invalid title. Please choose from the following: " + String.join(", ", validTitles));
+            }
+        }
 
         // Scale Point
         int scalePoint = readInt("Enter scale point:");
