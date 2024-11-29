cf118f09e89cd36dde205233736f676423e03021
endabuckley <endabuckley01@gmail.com>
2024-11-29 19:12:02 +0000

    Made a separate class for titles to clean up admin class

--- src/main/java/com/mycompany/payrollsystem/system/user/Admin.java
+++ src/main/java/com/mycompany/payrollsystem/system/user/Admin.java
@@ -1,6 +1,5 @@
 package com.mycompany.payrollsystem.system.user;
 
-
 import com.mycompany.payrollsystem.staff.FullTimeEmployee;
 import com.mycompany.payrollsystem.staff.PartTimeEmployee;
 import com.mycompany.payrollsystem.staff.Staff;
@@ -38,25 +37,7 @@ public class Admin {
     }
 
     public void addStaff() {
-        //placeholder arraylist, possibly a better way to do this by reading from csv files
-        List<String> validTitles = Arrays.asList(
-                "Full Professor","Professor","Associate Professor A","Associate Professor B",
-                "Assistant Professor","Teaching Assistant","Senior Administrative Officer III",
-                "Senior Administrative Officer II","Senior Administrative Officer I","Senior Executive Administrator",
-                "Executive Administrator","Senior Administrator","Administrator","EPS Portfolio Manager",
-                "EPS Category Manager","EPS Category Specialist Higher",
-                "EPS Category Specialist","Sub Librarian","Assistant Librarian II","Assistant Librarian I","Senior Library Assistant",
-                "Library Assistant","Analyst Programmer III","Analyst Programmer II",
-                "Analyst Programmer I","Senior Computer Operator","Computer Operator","Print Operator II","Print Operator I","Computer Lab Attendant",
-                "Temporary Computer Assistant","Chief Technical Officer",
-                "Technical Officer","Senior Technical Officer","Senior Lab Attendant","Laboratory Attendant","Sen Porter/Attendant","Porter/Attendant",
-                "Grounds Supervisor","Groundsworkperson","Senior Aide","Groundsworkperson Machine Attendant","Service Staff","Service Staff Shift",
-                "Plant Maintenance Aide","Grounds Foreperson","Teaching Fellow","University Teacher","Associate Teacher",
-                "Therapies Regional Supervisors Regional Placement Facilitator","Clinical Tutor","Clinical Fellow","Assistant Senior Instructor",
-                "Lead Instructor","Multi Activity Instructor (Grade I)","Multi Activity Instructor (Grade II)","Multi Activity Instructor (Grade III)",
-                "Assistant Instructor","CO-OP STUDENTS");
-
-
+        List<String> validTitles = Titles.getTitles();
 
         // Staff Type
         int type = readInt("Enter staff type: (1) full-time (2) part-time:");
@@ -67,7 +48,7 @@ public class Admin {
 
         // Name
         String name = readString("Enter staff name:");
-
+        
         // Unique ID
         int id;
         while (true) {
@@ -89,7 +70,7 @@ public class Admin {
         while (true) {
             title = readString("Enter staff title:").trim();
 
-            if (validTitles.contains(title)) {
+            if (Titles.getTitles().contains(title)) {
                 System.out.println("Title accepted: " + title);
                 break;
             } else {
